6edd38cb9317fed80b333f83d9b6ec2e3090b29c
frappuccino2316 <frappuccino2316@gmail.com>
2022-06-07 01:33:34 +0900

    おみくじ

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
-	s := sc.Text()
-	fmt.Println(s)
+	s := len(sc.Text())
+
+	fortune := [...]string{"大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"}
+
+	dt := time.Now()
+	u := int(dt.Unix() % 20)
+	fmt.Println(fortune[((s+u)*3-4)%7])
 }
