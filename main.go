863cc8cd81078dd8fb699970dbc8b66e1a10f224
frappuccino2316 <frappuccino2316@gmail.com>
2022-06-09 00:36:45 +0900

    err

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
 	"os"
-	"time"
 )
 
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Scan()
-	s := len(sc.Text())
+	// sc := bufio.NewScanner(os.Stdin)
+	// sc.Scan()
+	// s := len(sc.Text())
 
-	fortune := [...]string{"大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"}
-
-	dt := time.Now()
-	u := int(dt.Unix() % 20)
-	fmt.Println(fortune[((s+u)*3-4)%7])
+	fmt.Fprintln(os.Stderr, "エラー")
+	fmt.Fprintln(os.Stdout, "標準")
+	os.Exit(3)
 }
